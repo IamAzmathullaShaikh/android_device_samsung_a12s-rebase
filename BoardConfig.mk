77be76c91129c7f78b89ac3f2e23beb736dd9ab5
RiskyGUY22 <thomas_turner@talktalk.net>
2024-02-09 16:29:29 +0000

    Changed defconfig to use

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ DEVICE_PATH := device/samsung/a21sks
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
 ## Kernel
-TARGET_KERNEL_CONFIG := physwizz_defconfig
+TARGET_KERNEL_CONFIG := lineage_defconfig
 
 ## Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
