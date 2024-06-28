1d50071a4b11a61f58dce47d707ba7f43a757866
Azmathulla Shaik <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-06-28 20:00:42 +0530

    Add & Edit : a12s Variables

--- lineage_a12s.mk
+++ lineage_a12s.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 ## Inherit from a21s device
-$(call inherit-product, device/samsung/a21s/device.mk)
+$(call inherit-product, device/samsung/a12s/device.mk)
 
 ## Boot Animation
 TARGET_SCREEN_HEIGHT := 1600
@@ -33,26 +33,27 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_DEVICE := a21s
-PRODUCT_NAME := lineage_a21s
+PRODUCT_DEVICE := a12s
+PRODUCT_NAME := lineage_a12s
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A217F
+PRODUCT_MODEL := SM-A127F
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-PRODUCT_PDA_MODEL := A217F
-PRODUCT_PDA_MODEL_VERSION := XXSADWI1
-PRODUCT_PDA_VERSION := $(PRODUCT_PDA_MODEL)$(PRODUCT_PDA_MODEL_VERSION)
+#PRODUCT_PDA_MODEL := A127F
+#PRODUCT_PDA_MODEL_VERSION := XXSADWI1
+#PRODUCT_PDA_VERSION := $(PRODUCT_PDA_MODEL)$(PRODUCT_PDA_MODEL_VERSION)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=a21s \
-    PRODUCT_DEVICE=a21s \
-    PRIVATE_BUILD_DESC="a21s-user 12 SP1A.210812.016 $(PRODUCT_PDA_VERSION) release-keys"
+    PRODUCT_NAME=a12s \
+    PRODUCT_DEVICE=a12s
 
-BUILD_FINGERPRINT := samsung/a21snsxx/a21s:12/SP1A.210812.016/$(PRODUCT_PDA_VERSION):user/release-keys
+#\PRIVATE_BUILD_DESC="a21s-user 12 SP1A.210812.016 $(PRODUCT_PDA_VERSION) release-keys"
 
-PRODUCT_PRODUCT_PROPERTIES += \
+#BUILD_FINGERPRINT := samsung/a21snsxx/a21s:12/SP1A.210812.016/$(PRODUCT_PDA_VERSION):user/release-keys
+
+#PRODUCT_PRODUCT_PROPERTIES += \
     ro.build.PDA=$(PRODUCT_PDA_VERSION)
 
 ifneq ($(LINEAGE_DEV_CERTIFICATE),)
